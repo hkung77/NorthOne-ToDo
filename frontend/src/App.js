ef22e20978c06f83c1eff54353e6ce121c4ba2da
Henry Kung <henrykung@live.ca>
2021-01-27 15:57:49 -0500

    create skeleton for project

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from "./components/Home/index";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
