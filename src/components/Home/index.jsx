514bef74024fc3eb20807d33c632f0242477e18d
Henry Kung <henrykung@live.ca>
2021-02-01 09:06:18 -0500

    add styling to spinner

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -69,9 +69,11 @@ const Home = () => {
             setEditTask={setEditTask}
           />
         ) : (
-          <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-          </Spinner>
+          <div className="d-flex justify-content-center mt-5 pt-5">
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          </div>
         )}
       </Container>
       {editTask >= 0 ? (
